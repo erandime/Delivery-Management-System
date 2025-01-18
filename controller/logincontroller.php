293dfbde72d09b270af1ad6f8f06b060fc483d3f
erandime <epmadawalagama@gmail.com>
2025-01-18 20:19:16 +0800

    Sanitize user input to prevent SQLi or XSS.

--- controller/logincontroller.php
+++ controller/logincontroller.php
@@ -12,8 +12,9 @@
     $status = $_GET["status"];
     switch($status) {
        case "login": 
-           $username = $_POST["username"];
-           $password = $_POST["password"];
+           // Sanitize user inputs to prevent SQL injection and XSS
+        $username = htmlspecialchars(trim($_POST["username"]), ENT_QUOTES, 'UTF-8');
+        $password = htmlspecialchars(trim($_POST["password"]), ENT_QUOTES, 'UTF-8');
            
            try {
                if($username=="") {
