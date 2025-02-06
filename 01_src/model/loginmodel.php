850f3220fb3bf930188eab58dd3710555ca01423
erandime <epmadawalagama@gmail.com>
2025-02-07 03:15:21 +0800

    Use prepared statements to prevent SQLi

--- 01_src/model/loginmodel.php
+++ 01_src/model/loginmodel.php
@@ -5,12 +5,16 @@ $dbConnectionObj = new dbConnection(); //Create new dbconnection object
 
 class login{
     public function validateLogin($loginusername, $loginpassword) {
-        $con = $GLOBALS["con"];
+        $con = $GLOBALS["con"]; 
         $loginpassword = sha1($loginpassword);
         
         $sql = "SELECT l.login_username, l.login_password, u.user_name FROM login l, user u WHERE l.user_id = u.user_id "
-                . "AND l.login_username = '$loginusername' AND l.login_password = '$loginpassword'";
-        $result = $con->query($sql) or die ($con->error); //Run query or display error
+                . "AND l.login_username = ? AND l.login_password = ?"; //Secure the query using prepared statements to prevent SQLi
+        $stmt = $con->prepare($sql); //Prepare the SQL statement
+        $stmt->bind_param("ss", $loginusername, $loginpassword);    //Bind parameters
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
         return $result;
     }
 }
